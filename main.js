d23530299fc8e31dbd9b6065e8c7c9af8343fd0e
wassimxd <karimmohsin10@gmail.com>
2023-04-07 13:55:25 +0000

    third day

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 // page mode
 function darkMode() {
   let element = document.body;
+  let font = document.querySelectorAll(".fa-moon")[0];
   element.dataset.bsTheme =
     element.dataset.bsTheme == "light" ? "dark" : "light";
+  if (element.dataset.bsTheme == "dark") {
+    font.classList.add("fa-solid");
+    font.classList.remove("fa-regular");
+  } else {
+    font.classList.remove("fa-solid");
+    font.classList.add("fa-regular");
+  }
 }
